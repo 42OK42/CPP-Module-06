a59d70249c2878531351dccc5e553bb302d2bc48
OK42OK <krahlolli@gmail.com>
2024-12-09 17:25:32 +0100

    added some print statements

--- ex02/src/generate.cpp
+++ ex02/src/generate.cpp
@@ -6,7 +6,7 @@
 /*   By: okrahl <okrahl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:56:48 by okrahl            #+#    #+#             */
-/*   Updated: 2024/12/09 16:56:51 by okrahl           ###   ########.fr       */
+/*   Updated: 2024/12/09 17:24:50 by okrahl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,27 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <cstdlib>
+#include <iostream>
 #include <ctime>
 
 // Randomly instantiate A, B, or C and return as a Base pointer
 Base* generate() {
 	std::srand(std::time(0)); // Seed for random number generation
 	int random = std::rand() % 3;
+	Base* result = NULL;
 
-	if (random == 0)
-		return new A();
-	else if (random == 1)
-		return new B();
-	else
-		return new C();
+	if (random == 0) {
+		result = new A();
+		std::cout << "Generated class A" << std::endl;
+	}
+	else if (random == 1) {
+		result = new B();
+		std::cout << "Generated class B" << std::endl;
+	}
+	else {
+		result = new C();
+		std::cout << "Generated class C" << std::endl;
+	}
+
+	return result;
 } 
